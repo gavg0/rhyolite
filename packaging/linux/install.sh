2b806d3ffc7fefb6483579b4719f676f7a64dfd5
Suyog Tandel <127536688+RedddFoxxyy@users.noreply.github.com>
2024-12-23 00:29:34 +0530

    Update install.sh

--- packaging/linux/install.sh
+++ packaging/linux/install.sh
@@ -5,7 +5,7 @@ set -e
 APP_NAME="Rhyolite"   # Replace with your app's name
 BINARY_URL="https://github.com/RedddFoxxyy/Rhyolite/releases/download/v0.1.1/Rhyolite_0.1.1_linux_binary"  # Update with your binary's URL
 ICON_URL="https://github.com/RedddFoxxyy/Rhyolite/blob/master/src-tauri/icons/icon.png"  # Update with your app's icon URL
-INSTALL_DIR="/usr/local/bin"
+INSTALL_DIR="$HOME/.local/bin/Rhyolite"
 DESKTOP_FILE_DIR="$HOME/.local/share/applications" # For a user-specific install
 ICON_DIR="$HOME/.local/share/icons"
 
@@ -16,16 +16,16 @@ curl -L $BINARY_URL -o /tmp/$APP_NAME
 chmod +x /tmp/$APP_NAME
 
 # Move the binary to the installation directory
-sudo mv /tmp/$APP_NAME $INSTALL_DIR/$APP_NAME
+mv /tmp/$APP_NAME $INSTALL_DIR/$APP_NAME
 
 # Download and install the icon
-sudo mkdir -p $ICON_DIR
+mkdir -p $ICON_DIR
 curl -L $ICON_URL -o /tmp/$APP_NAME.png
-sudo mv /tmp/$APP_NAME.png $ICON_DIR/$APP_NAME.png
+mv /tmp/$APP_NAME.png $ICON_DIR/$APP_NAME.png
 
 # Create a .desktop file
-sudo mkdir -p $DESKTOP_FILE_DIR
-sudo DESKTOP_FILE=$DESKTOP_FILE_DIR/$APP_NAME.desktop
+mkdir -p $DESKTOP_FILE_DIR
+DESKTOP_FILE=$DESKTOP_FILE_DIR/$APP_NAME.desktop
 
 cat <<EOF > $DESKTOP_FILE
 [Desktop Entry]

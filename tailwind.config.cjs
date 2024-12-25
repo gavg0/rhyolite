184f8905fc3462fc6585c9a78d54655ce5396db3
prettyblueberry <121964338+prettyblueberry@users.noreply.github.com>
2024-12-25 10:16:55 +0100

    install flowbite

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
+import flowbitePlugin from 'flowbite/plugin'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{html,js,svelte,ts}', // Ensure all Svelte files are included
+    './src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
   ],
+  darkMode: 'selector',
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbitePlugin],
 };
 

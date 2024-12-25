9deeabbd3d271e2332817c445f1ff9a75203f5c9
prettyblueberry <121964338+prettyblueberry@users.noreply.github.com>
2024-12-25 10:10:34 +0100

    install tailwind-css

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,svelte,ts}', // Ensure all Svelte files are included
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+

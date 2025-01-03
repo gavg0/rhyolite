8c2b8b78a189777e926fd805c4a1e2f99e6272c2
Suyog Tandel <127536688+RedddFoxxyy@users.noreply.github.com>
2025-01-03 11:58:23 +0530

    add flowbite dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite-typography'),
+    require('flowbite/plugin')({
+      wysiwyg: true,
+    }),
+  ],
+}
+

9a11f4965473d2cef54dc6b81f432c8d4aae9d47
Suyog Tandel <127536688+RedddFoxxyy@users.noreply.github.com>
2025-01-09 12:53:35 +0530

    restore default theme css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
     import "../app.css";
+    import "../styles/themes/rhyolite-dark.css";
 </script>
 
 <slot />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,32 +2,31 @@
 
 export default {
   content: [
-    './src/**/*.{html,js,svelte,ts}',
-    "./node_modules/flowbite/**/*.js"
+    "./src/**/*.{html,js,svelte,ts}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     colors: {
-        text: '#ffffff',
-        subtext2: '#f1f2f3',
-        subtext1: '#d8dbde',
-        subtext0: '#c2c6cb',
-        overlay2: '#acb2b8',
-        overlay1: '#969da5',
-        overlay0: '#808992',
-        surface2: '#6c757d',
-        surface1: '#596167',
-        surface0: '#464c51',
-        base: '#33373b',
-        crust: '#202325',
-        mantle: '#0d0e0f'
+      text: "var(--color-text)",
+      subtext2: "var(--color-subtext2)",
+      subtext1: "var(--color-subtext1)",
+      subtext0: "var(--color-subtext0)",
+      overlay2: "var(--color-overlay2)",
+      overlay1: "var(--color-overlay1)",
+      overlay0: "var(--color-overlay0)",
+      surface2: "var(--color-surface2)",
+      surface1: "var(--color-surface1)",
+      surface0: "var(--color-surface0)",
+      base: "var(--color-base)",
+      crust: "var(--color-crust)",
+      mantle: "var(--color-mantle)",
     },
     extend: {},
   },
   plugins: [
-    require('flowbite-typography'),
-    require('flowbite/plugin')({
+    require("flowbite-typography"),
+    require("flowbite/plugin")({
       wysiwyg: true,
     }),
   ],
-}
-
+};

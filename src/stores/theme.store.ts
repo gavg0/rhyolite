1b17f30ce1198d8dc1db84ff071f8d3bd0d93ea4
Avinash Thakur <iamavinashthakur.at@gmail.com>
2025-01-14 02:00:31 +0530

    feat: add coffee and rename green screen theme

--- src/stores/theme.store.ts
+++ src/stores/theme.store.ts
@@ -27,7 +27,7 @@ const DEFAULT_THEMES: Theme[] = [
     },
   },
   {
-    name: "Green",
+    name: "Green Screen",
     colorscheme: "dark",
     colors: {
       text: "#ffffff",
@@ -43,6 +43,25 @@ const DEFAULT_THEMES: Theme[] = [
       base: "#335f3b",
       crust: "#203325",
       mantle: "#081d08",
+    },
+  },
+  {
+    name: "Coffee",
+    colorscheme: "light",
+    colors: {
+      text: "#FFDBB5",
+      subtext2: "#FFEAC5",
+      subtext1: "#d8dbde",
+      subtext0: "#c2c6cb",
+      overlay2: "#acb2b8",
+      overlay1: "#969da5",
+      overlay0: "#808992",
+      surface2: "#a07349",
+      surface1: "#845f3c",
+      surface0: "#684b2f",
+      base: "#4c3622",
+      crust: "#2f2215",
+      mantle: "#130e09",
     },
   },
 ];

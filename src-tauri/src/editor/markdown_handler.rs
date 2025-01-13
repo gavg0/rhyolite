4935f116b224b21889d1d1741542e398faf84ff7
Suyog Tandel <127536688+RedddFoxxyy@users.noreply.github.com>
2025-01-13 22:45:31 +0530

    update cargo packages

--- src-tauri/src/editor/markdown_handler.rs
+++ src-tauri/src/editor/markdown_handler.rs
@@ -1,7 +1,5 @@
-// use html2md::parse_html; //html2md module to convert html to markdown
-use markdown_engine::convert_to_markdown; //markdown_engine module to convert markdown to html
+use markdown_engine::convert_to_markdown; //markdown_engine module to convert html to markdown
 use pulldown_cmark::{html, Options, Parser}; //pulldown_cmark module to parse markdown
-// use mdka; //mdka module to convert markdown to html
 use regex::Regex; //regex module to use Regex type
 
 pub fn html_to_markdown(html: &str) -> String {

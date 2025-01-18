765c2d039bcd8c40b4877ea895831edd161c8d12
ShravyaKudlu <shravyakshanbhogue@gmail.com>
2025-01-18 18:18:54 -0500

    titlebarUpdates

--- src/components/titlebar.svelte
+++ src/components/titlebar.svelte
@@ -9,9 +9,6 @@
   import { type Tab } from "../types/tab";
   import { addNewDocumentTab } from "../services/document.service";
   import closeTab from "../services/tab.service";
-  import ThemeStore from "../stores/theme.store";
-  import type { Theme } from "../types/theme";
-  import { Select } from "flowbite-svelte";
 
   let tabs: Tab[] = $state([]);
   let currentTab: Tab | null = $state(null);
@@ -20,14 +17,9 @@
 
   const appWindow = getCurrentWindow();
 
-  // Initial state
-  // getCurrentWindow()
-  //   .isMaximized()
-  //   .then((result) => {
-  //     isMaximized = result;
-  //   });
+  let hoverTabId: String | null = $state(null);
 
-  // Listen for maximize and unmaximize events
+
   appWindow.listen("tauri://resize", async () => {
     isMaximized = await appWindow.isMaximized();
   });
@@ -71,20 +63,34 @@
     id="tablist"
     aria-label="Document tabs"
   >
-    {#each tabs as tab}
-      <button
-        class={`flex justify-left items-center px-4 text-nowrap h-[30px] min-w-[120px] rounded-[18px] flex-shrink text-text hover:bg-surface1 ${currentTab?.id === tab.id ? "bg-surface0" : ""}`}
-        class:active={currentTab?.id === tab.id}
-        role="tab"
-        aria-controls="editor"
-        onclick={() => onOpenTab(tab)}
-      >
-        {tab.title.length > 20
-          ? tab.title.slice(0, 20) + "..."
-          : tab.title || "Untitled"}
-      </button>
-    {/each}
-
+  {#each tabs as tab}
+  <div class="relative group"> 
+    <button
+      class={`flex justify-left items-center px-4 text-nowrap h-[30px] min-w-[120px] rounded-[18px] flex-shrink text-text hover:bg-surface1 ${currentTab?.id === tab.id ? "bg-surface0" : ""} group-hover:bg-surface1`}
+      class:active={currentTab?.id === tab.id}
+      role="tab"
+      aria-controls="editor"
+      onclick={() => onOpenTab(tab)}
+      onmouseenter={() => (hoverTabId = tab.id)}
+      onmouseleave={() => (hoverTabId = null)}
+    >
+      {tab.title.length > 20
+        ? tab.title.slice(0, 20) + "..."
+        : tab.title || "Untitled"}
+    </button>
+    <button
+    class="absolute right-2 top-1/2 transform -translate-y-1/2 text-text bg-base rounded-[18px] h-[20px] w-[20px] flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-200 hover:bg-red-500 hover:text-white"
+    class:opacity-100={currentTab?.id === tab.id || hoverTabId === tab.id}
+      onclick={(e) => {
+        e.stopPropagation(); 
+        console.log(`Closed tab: ${tab.title}`); 
+      }}
+    >
+      X
+    </button>
+  </div>
+  
+{/each}
     <button
       type="button"
       class="flex justify-center items-center px-4 text-nowrap h-[30px] w-[30px] aspect-square rounded-[18px] flex-shrink text-text hover:bg-surface1"
